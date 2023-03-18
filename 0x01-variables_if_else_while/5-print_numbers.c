1f0f03251fbcab64c61845769d9079f148ea1a4e
rajae <hemmanyrajae@gmail.com>
2023-03-17 19:51:19 -0700

    r

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: digits base 10.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		printf("%d", dig));
+		dig++;
+	}
+	printf("\n");
+
+	return (0);
+}
