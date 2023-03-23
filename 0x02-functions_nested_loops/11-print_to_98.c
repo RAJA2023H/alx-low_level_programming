14ff742f59002db20ea68f5ff30074fe01583bc1
rajae <hemmanyrajae@gmail.com>
2023-03-22 19:40:22 -0700

    g

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98.
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 0; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
