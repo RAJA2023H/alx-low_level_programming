b6fab33129510e772571e20310ee7733897a6e2e
rajae <hemmanyrajae@gmail.com>
2023-03-23 14:20:38 -0700

    g

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	while (i < 10)/*i never increments*/
 	{
-		putchar(i);/*i never increments*/
+		putchar(i);
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
