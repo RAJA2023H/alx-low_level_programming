a9c7d2bf8957aab266f2ead9ddf6895862fff63e
rajae <hemmanyrajae@gmail.com>
2023-03-25 11:51:03 -0700

    f

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Description:  prints the numbers from 1 to 100, print Fizz, print buzz.
+ * Return: Always 0 (Success).
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && !(n % 5 == 0))
+		{
+			printf("Fizz");
+		}
+		else if (!(n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("Buzz");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		if (n != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+
+	return (0);
+}
