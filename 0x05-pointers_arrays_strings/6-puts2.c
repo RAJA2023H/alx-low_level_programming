b4f1fac44180177be5330104709d5d3b75340e51
rajae <hemmanyrajae@gmail.com>
2023-03-28 19:18:20 -0700

    f

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string,
+ * starting with the first character.
+ * @str: input string.
+*/
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (((str[len]) + 48) % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+		len++;
+	}
+}
