d4e3e9f0a0153e654bf2f1191efa65f0140a38b6
rajae <hemmanyrajae@gmail.com>
2023-03-31 18:39:59 -0700

    g

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,20 +12,19 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i - 1] == 32 || str[i - 1] == 9
-		|| str[i - 1] == 10 || str[i - 1] == 44
-		|| str[i - 1] == 59 || str[i - 1] == 46
-		|| str[i - 1] == 33 || str[i - 1] == 63
-		|| str[i - 1] == 34 || str[i - 1] == 40
-		|| str[i - 1] == 41 || str[i - 1] == 123
-		|| str[i - 1] == 125)
+		if (str[i] <= 'z' && str[i] >= 'a')
 		{
-			if (str[i] <= 122 && str[i] >= 97)
+			if (str[i - 1] == ' ' || str[i - 1] == '\t'
+			|| str[i - 1] == '\n' || str[i - 1] == ','
+			|| str[i - 1] == ';' || str[i - 1] == '.'
+			|| str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '('
+			|| str[i - 1] == ')' || str[i - 1] == '{'
+			|| str[i - 1] == '}' || i == 0)
 			{
 				str[i] -= 32;
 			}
 		}
 	}
-	str[i] = '\0';
 	return (str);
 }
