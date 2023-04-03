4e018ba5204c5300ff23aeb55aa95a043ec1200b
rajae <hemmanyrajae@gmail.com>
2023-04-02 21:12:43 -0700

    c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input number.
+*/
+
+void print_number(int n)
+{
+	unsigned int x, fac, num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = n * -1;
+	}
+	else
+	{
+		x = n;
+	}
+
+	num = x;
+	for (fac = 1; num > 9; fac *= 10)
+	{
+		num /= 10;
+	}
+	for (; fac >= 1; fac /= 10)
+	{
+		_putchar(((x / fac) % 10) + 48);
+	}
+}
