378def4653fcfd31060e1b215f26947e5a3f3aea
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-04 18:49:59 -0700

    h

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	unsigned int len;
+	int i, j;
+	unsigned int len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (len = 0; accept[len] != '\0'; len++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[len])
+			if (s[i] == accept[j])
 			{
 				len++;
 				break;
 			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (len);
 		}
 	}
-	len += 1;
 	return (len);
 }
