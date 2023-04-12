3c5043a73bb2e064a50d3f726808dfc332891358
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-12 16:07:35 -0700

    f

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,13 +23,16 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			for (j = 0 ; j < width ; j++)
+			for (; i >= 0; i--)
 				free(grid[i]);
 			free(grid);
 			return (NULL);
 		}
 	}
-	for (i = 0 ; i < height ; i++)
-		**grid = 0;
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
 	return (grid);
 }
