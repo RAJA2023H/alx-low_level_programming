54eb5c694838697f4b2d9279d0f33847e5e59302
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-13 14:36:29 -0700

    f

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,16 @@ int main(int argc, char *argv[])
 
 	if (argc > 3 || argc < 3)
 	{
+		printf("Error\n");
+		return(1);
+	}
+	else
+	{
 		for (i = 1; i < argc; i++)
 		{
 			m *= atoi(argv[i]);
 		}
 		printf("%d\n", m);
 	}
-	else
-	{
-		printf("Error\n");
-	}
-
 	return (0);
 }
