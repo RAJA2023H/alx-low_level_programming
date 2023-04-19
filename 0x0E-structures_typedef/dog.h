9c58e3647438af30f72b68219dfc3336d3c76492
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-19 05:07:16 -0700

    g

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type 
+ * @name: pointer to string
+ * @age:  float
+ * @owner: string
+ */
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+#endif
