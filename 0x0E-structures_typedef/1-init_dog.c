655d6944d448b68889634eb3d6a8e02053527d2d
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-19 06:23:01 -0700

    f

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,6 @@
  * @name: input string
  * @age: input float
  * @owner: input string
- * Description: initialize a variable of type struct dog
  *
  * Return: void
  */
@@ -15,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
