850f8e790c457d299ec00e043d40e31c9a423daa
RAJA2023H <hemmanyrajae@gmail.com>
2023-04-20 16:06:19 -0700

    d

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ * get_op_func -  perform the asked  operation
+ * @s: pointer to string
+ * Return: pointer a function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {{"+", op_add}, {"-", op_sub},
+			{"*", op_mul}, {"/", op_div},
+			{"%", op_mod}, {NULL, NULL}};
+	int x = 0;
+
+	while (ops[x].op)
+	{
+		if (!strcmp(ops[x].op, s))
+			return (ops[x].f);
+		x++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - entry point
+ * @a: int
+ * @b: int
+ * Return: 0 when success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entry point
+ * @a: int
+ * @b: int
+ * Return: 0 when success
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - entry point
+ * @a: int
+ * @b: int
+ * Return: 0 when success
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - entry point
+ * @a: integer
+ * @b: integer
+ *
+ * Return: 0 when success
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - entry point
+ * @a: int
+ * @b: int
+ * Return: 0 when success
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
