ff727c4d0513e1bf57a036b5266c7e7855f1f973
RAJA2023H <hemmanyrajae@gmail.com>
2023-06-10 16:25:51 -0700

    g

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that counts arguments..
+ *
+ * @argc : int
+ *
+ * @argv : string
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char const *argv[])
+{
+    (void)argv;
+    printf("%d\n", argc - 1);
+    return (0);
+}
+
