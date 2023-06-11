cfcf9fa5c9b4ae24ecf407c965db5077d234a1c4
RAJA2023H <hemmanyrajae@gmail.com>
2023-06-10 17:20:56 -0700

    f

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,10 +15,10 @@ int main(int argc, char *argv[])
 
 	while (--argc)
 	{
-		for(c = argv[argc]; *c; c++)
+		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n"),1);
-		s+= atoi(argv[argc]);
+				return (printf("Error\n"), 1);
+		s += atoi(argv[argc]);
 	}
 	printf("%d\n", s);
 	return (0);
