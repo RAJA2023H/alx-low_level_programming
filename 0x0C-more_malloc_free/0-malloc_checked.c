20958c6792ca6fe73c867942c76d32ff199b68e2
RAJA2023H <hemmanyrajae@gmail.com>
2023-06-16 13:31:15 -0700

    testing the code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: first bytes of the memory.
+ * Return : pointer or 98 if fails.
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+	
+	return (s);
+}
