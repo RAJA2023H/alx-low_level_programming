fa300a7d4b4cc74e27b89d151fa8dc00109d9780
RAJA2023H <hemmanyrajae@gmail.com>
2023-06-17 12:39:19 -0700

    betty's errors

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,12 +14,11 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		return (-1);
-	}/*If y is lower than 0, the function should return -1*/
- 	if (y == 0)
- 	{
- 		return (1);
- 	}
- 
+	} /*If y is lower than 0, the function should return -1*/
+	if (y == 0)
+	{
+		return (1);
+	}
 	return (x * _pow_recursion(x, y - 1));
 	/*returns the value of x raised to the power of y*/
 }
