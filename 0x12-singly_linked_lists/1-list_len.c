562ec932234c5ce141d54f617228cb1bdb29aea8
RAJA2023H <hemmanyrajae@gmail.com>
2023-06-30 20:22:15 -0700

    first commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list.
+ * @h: pointer to head of a linked list
+ * Return: returns the number of elements in a linked list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
