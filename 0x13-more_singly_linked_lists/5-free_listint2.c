06b27cc261a00bf07b3cce8bab3eb9060d9f3548
RAJA2023H <hemmanyrajae@gmail.com>
2023-07-04 18:32:19 -0700

    checker6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *next;
 
-	if (head == NULL)
-		return;
-	while (current != NULL)
+	if (head != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		current = *head;
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
