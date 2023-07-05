b59984d6cc4ae31f8eb2751f6e8e32649885ea2e
RAJA2023H <hemmanyrajae@gmail.com>
2023-07-04 17:16:15 -0700

    betty errors

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer to head
+ * Return: void.
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+
+		free(current);
+		current = next;
+	}
+}
