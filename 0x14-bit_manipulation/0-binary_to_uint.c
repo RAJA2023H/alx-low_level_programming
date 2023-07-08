be373a944718f0adf0fee1a7f2ccec961c3ecb39
RAJA2023H <hemmanyrajae@gmail.com>
2023-07-08 07:22:48 -0700

    fixing checker 9 by addin the hesder stddef lib

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	size_t len = strlen(b);
-	unsigned int power = 1;
-	unsigned int result = 0;
+	unsigned int power;
+	unsigned int result;
 	int i;
 
 	if (b == NULL)
 		return (0);
-
+	result = 0;
+	power = 1;
 	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] == '0')
