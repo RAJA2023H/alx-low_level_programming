7a9fadbfb5e47bb0e0c6eca88b6f055a3cf35d0a
RAJA2023H <hemmanyrajae@gmail.com>
2023-08-31 15:43:40 -0700

    first commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * set_bit - set a bit's value to 1
+ *
+ * @n: pointer to number
+ * @index: index that start from 0
+ *
+ * Return: 1 on success || -1 on fail
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int ibit;
+
+	ibit = (*n & (1 << index)) ? 1 : 0;
+	if (ibit == 0)
+	{
+		*n = *n ^ (1 << index);
+	}
+	return (*n);
+}
