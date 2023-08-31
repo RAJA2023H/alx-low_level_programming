5ffd62b52fd0e8ff05dcacda6b9cc6872aac79c7
RAJA2023H <hemmanyrajae@gmail.com>
2023-08-31 15:55:01 -0700

    fixing the retune value

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,10 +12,13 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int ibit;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	ibit = (*n & (1 << index)) ? 1 : 0;
 	if (ibit == 0)
 	{
 		*n = *n ^ (1 << index);
 	}
-	return (*n);
+	return (1);
 }
