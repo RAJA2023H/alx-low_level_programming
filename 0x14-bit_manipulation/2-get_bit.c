c22919368bed2c97c2d942c150a431ad136a61ab
RAJA2023H <hemmanyrajae@gmail.com>
2023-08-31 15:23:01 -0700

    first commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - return bit's value
+ *
+ * @n: pointer number
+ * @index: index that start from 0
+ *
+ * Return: 1 on success || -1 on fail
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int ibit;
+
+	ibit = (n & (1 << index)) ? 1 : 0;
+	return (ibit);
+}
