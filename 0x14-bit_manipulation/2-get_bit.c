67d2c99acedca26d6f6e628454983d3e5c1751d0
RAJA2023H <hemmanyrajae@gmail.com>
2023-08-31 15:30:48 -0700

    fixing data type

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int ibit;
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
 
 	ibit = (n & (1 << index)) ? 1 : 0;
 	return (ibit);
