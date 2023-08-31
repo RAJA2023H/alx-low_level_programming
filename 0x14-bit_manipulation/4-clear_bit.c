7d0540bb72109e3b01ea366c0e059b874f37c714
RAJA2023H <hemmanyrajae@gmail.com>
2023-08-31 16:09:18 -0700

    first commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit's value to 0
+ *
+ * @n: pointer of number
+ * @index: bit index to set on
+ *
+ * Return: 1 on success || -1 on fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int ibit;
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	ibit = (*n & (1 << index)) ? 1 : 0;
+	if( ibit == 1)
+	{
+		*n = *n ^ (1 << index);
+	}
+	return (1);
+}
