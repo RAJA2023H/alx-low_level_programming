b77b2220c41c044b71b24b5585e33c35bdc267f8
RAJA2023H <hemmanyrajae@gmail.com>
2023-09-04 16:44:45 -0700

    return 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,34 +14,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		return (-1);
+		return (0);
 	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(fd);
 		free(buffer);
-		return (-1);
+		return (0);
 	}
 	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_write == -1)
 	{
 		close(fd);
 		free(buffer);
-		return (-1);
+		return (0);
 	}
 	close(fd);
 	free(buffer);
