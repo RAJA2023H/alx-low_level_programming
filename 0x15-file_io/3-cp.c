621756e048a7f81e204295425ea8fb4decfcbc8b
RAJA2023H <hemmanyrajae@gmail.com>
2023-09-04 18:46:20 -0700

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * error_file - checks if files can be opened.
+ * error - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
 
-void error_file(int file_from, int file_to, char *argv[])
+void error(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -29,7 +29,7 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
+	int file_from, file_to, error_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
@@ -41,27 +41,27 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	error(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
+			error(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			error_file(0, -1, argv);
+			error(0, -1, argv);
 	}
 
-	err_close = close(file_from);
+	error_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
+	error_close = close(file_to);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
