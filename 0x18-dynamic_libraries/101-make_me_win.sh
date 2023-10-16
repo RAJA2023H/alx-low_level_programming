7e1c732899d0ccd217463f8afc83657632c4a8b5
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-16 16:40:24 -0700

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-export LD_PRELOAD="/tmp/randomx.so"
+wget -N -P /tmp  https://raw.githubusercontent.com/RAJA2023H/alx-low_level_programming/tree/master/0x18-dynamic_libraries/rrondom.so
+export LD_PRELOAD="/tmp/rrondom.so"
