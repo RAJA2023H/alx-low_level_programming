2f410b1d83a79894a58f1d03fd9100237a240a91
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-17 16:29:06 -0700

    up

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -N -P /tmp  https://raw.githubusercontent.com/RAJA2023H/alx-low_level_programming/tree/master/0x18-dynamic_libraries/rrondom.so
+wget -N -P /tmp https://github.com/RAJA2023H/alx-low_level_programming/blob/b3b4c1c3cf551184cccda938905cc75a2ced733b/0x18-dynamic_libraries/rrondom.so
 export LD_PRELOAD="/tmp/rrondom.so"
