56726ffbc9f37802e9ce4b281beb57b6b626b266
rajae <hemmanyrajae@gmail.com>
2023-12-04 13:57:54 +0000

    up

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	if (!new_table->array)
 	{
-		free(table);
+		free(new_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
