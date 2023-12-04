726feef8db5df535a70b6c095f4bda0e1ecfdc3b
rajae <hemmanyrajae@gmail.com>
2023-12-04 10:17:39 +0000

    up

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,10 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	hash_table_t *new_table;
+
+	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (!size)
 	{
 		return (NULL);
@@ -22,7 +25,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return(NULL);
 	}
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		new_table->array[i] = NULL;
 	}
