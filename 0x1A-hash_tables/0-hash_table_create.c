bd21d1221f157523232025fcfb2b350447f51d8a
rajae <hemmanyrajae@gmail.com>
2023-12-22 13:15:15 +0000

    up

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 	{
 		return (NULL);
