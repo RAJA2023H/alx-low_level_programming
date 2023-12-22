ae7e9ee60542d12b2e85263332329929efbbb37f
rajae <hemmanyrajae@gmail.com>
2023-12-22 13:40:55 +0000

    up

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,23 +11,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!size)
 	{
-		return (NULL);
+		return(NULL);
 	}
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 	{
-		return (NULL);
+		return(NULL);
 	}
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * new_table->size);
 	if (!new_table->array)
 	{
 		free(new_table);
-		return (NULL);
+		return(NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
 		new_table->array[i] = NULL;
 	}
-	return (new_table);
+	return(new_table);
 }
