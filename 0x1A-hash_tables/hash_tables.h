32d5448db96b69b6b13663074d06a9948af7598a
rajae <hemmanyrajae@gmail.com>
2023-12-22 13:53:16 +0000

    up

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -18,7 +18,6 @@ typedef struct hash_node_s
 	char *value;
 	struct hash_node_s *next;
 } hash_node_t;
-
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -34,4 +33,4 @@ typedef struct hash_table_s
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-#endif 
+#endif
