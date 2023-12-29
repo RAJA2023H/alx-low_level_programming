b70ff9038fadf720519cab1809a6b3b31f404abd
rajae <hemmanyrajae@gmail.com>
2023-12-29 18:40:56 +0000

    up

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - returns you the index of a key
+ * @key: The key
+ * @size: The size of the array of the hash table
+ * Return: pointer to the newly created hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
