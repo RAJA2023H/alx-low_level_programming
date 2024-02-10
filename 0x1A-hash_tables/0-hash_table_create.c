265c6127bc8e52f6c582ab9980cc2f45faeb80a2
rajae <hemmanyrajae@gmail.com>
2024-02-10 03:34:20 +0000

    resolved

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,19 +11,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!size)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	hash_table->size = size;
 	hash_table->array =  malloc(sizeof(hash_node_t *) * hash_table->size);
 	if (!hash_table->array)
 	{
-		free(hash_table);
-		return(NULL);
+		return (NULL);
 	}
 	for (x = 0; x < size; x++)
 	{
