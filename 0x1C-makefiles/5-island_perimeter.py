e07f7b659a1961e64bb14f06062fa1347a67ee17
Rajae <hemmanyrajae@gmail.com>
2024-03-20 20:59:42 -0700

    up

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# function that returns the perimeter of the island described in grid
+def island_perimeter(grid):
+    P = 0
+    if len(grid) == 0:
+        return P
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                P += 4
+                if grid[row][col - 1] == 1:
+                    P-=1
+                if grid[row][col + 1] == 1:
+                    P-=1
+                if grid[row - 1][col] == 1:
+                    P-=1
+                if grid[row + 1][col] == 1:
+                    P-=1
+    return P
