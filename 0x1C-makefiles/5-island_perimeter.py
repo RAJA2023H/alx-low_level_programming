25bfe06ec68d3a88500be4ed83cbd18894cc38af
Rajae <hemmanyrajae@gmail.com>
2024-03-20 23:46:19 -0700

    up

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,10 +21,10 @@ def island_perimeter(grid):
                 P += 4
                 if (col - 1) >= 0 and grid[row][col - 1] == 1:
                     P -= 1
-                if (col + 1) <= cols and grid[row][col + 1] == 1:
+                if (col + 1) < cols and grid[row][col + 1] == 1:
                     P -= 1
                 if (row - 1) >= 0 and grid[row - 1][col] == 1:
                     P -= 1
-                if (row + 1) <= rows and grid[row + 1][col] == 1:
+                if (row + 1) <  rows and grid[row + 1][col] == 1:
                     P -= 1
     return P
