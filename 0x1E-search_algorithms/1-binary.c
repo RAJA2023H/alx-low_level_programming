71ec9b50c1617473b32e781b63aacdf10312052e
rajae <hemmanyrajae@gmail.com>
2024-05-19 19:18:54 -0700

    up

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+/**
+ * binary_search - function
+ * @array: pointer to the first elemen
+ * @size: number of elements in array
+ * @value: value to search for
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
+
+	if (!array)
+		return (-1);
+
+	while (left <= right)
+	{
+		middle = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (array[middle] == value)
+			return (middle);
+
+		if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
+
+	return (-1);
+}
